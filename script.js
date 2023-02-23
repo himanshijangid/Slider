b7972f8b74a4b199e624c7df65456be67d3eeb4d
Himanshi Jangid <himanshijangid444@gmail.com>
2023-02-23 16:08:19 +0530

    hfbf

---
+++ script.js
@@ -0,0 +1,27 @@
+let Index = 1;
+show_image(Index);
+
+
+
+
+function show_image(i){
+     i ;
+    Index += i;
+    let image =document.getElementsByClassName("image");
+    let dots = document.getElementsByClassName("dot");
+    if(i > image.length-1){
+        Index = 0;
+
+    }
+    if(Index < 1){
+        Index = image.length - 1;
+    }
+    for (i = 0 ; i < image.length ;  i++){
+        image[i].style.display = "none";
+    }
+    for(i = 0 ; i < dots.length ; i++){
+        dots[i].className = dots[i].className.replace("active","");
+    }
+    image[Index].style.display = "block";
+    dots[Index].style.className  = "active";
+    }
